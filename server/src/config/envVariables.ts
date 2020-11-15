2b5091c52e14565098386b71a0b5a927e674b17b
Cleomir <cleomir.mg@hotmail.com>
2020-11-15 11:39:14 +0200

    Add API keys

--- server/src/config/envVariables.ts
+++ server/src/config/envVariables.ts
@@ -4,8 +4,13 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 export const env: { [key: string]: string } = {
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
   NODE_ENV: process.env.NODE_ENV!,
+  PEXELS_API_KEY: process.env.PEXELS_API_KEY!,
   SERVER_PORT: process.env.SERVER_PORT!,
+  UNSPLASH_API_KEY: process.env.UNSPLASH_API_KEY!,
+  UNSPLASH_API_SECRET: process.env.UNSPLASH_API_SECRET!,
 };
 
 /**
