d72d1d5620bf8374502c1944e92d83ca0b2e5988
Cleomir <cleomir.mg@hotmail.com>
2020-11-15 20:38:27 +0200

    Add IEnvVariables

---
+++ server/src/interfaces/IEnvVariables.ts
@@ -0,0 +1,9 @@
+export default interface IEnvVariables {
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
+  NODE_ENV: string;
+  PEXELS_API_KEY: string;
+  SERVER_PORT: string;
+  UNSPLASH_API_KEY: string;
+  UNSPLASH_API_SECRET: string;
+}
