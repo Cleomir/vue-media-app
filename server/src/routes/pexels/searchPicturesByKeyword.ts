d7bfcb708af8224d4b796121cb96115da7191f62
Cleomir <cleomir.mg@hotmail.com>
2020-11-15 21:17:43 +0200

    Add Pexels search pictures by keyword

--- server/src/routes/pexels/searchPicturesByKeyword.ts
+++ server/src/routes/pexels/searchPicturesByKeyword.ts
@@ -1,12 +1,53 @@
 import { Request, Response } from "express";
+import axios, { AxiosResponse } from "axios";
+
+import logger, { logObject } from "../../logger";
+import { env } from "../../config/envVariables";
+import {
+  INTERNAL_SERVER_ERROR,
+  NO_SEARCH_KEYWORD,
+} from "../../config/responseErrorMessages";
 
 /**
- *
+ * Search pictures by the given keyword
  * @param req - Request object
  * @param res - Response object
  */
-const searchPicturesByKeyword = (req: Request, res: Response): Response => {
-  return res.status(200).send("Pexels search pictures");
+const searchPicturesByKeyword = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { id, query } = req;
+  if (!query.query) {
+    return res.status(400).json({ success: false, message: NO_SEARCH_KEYWORD });
+  }
+
+  const requestParams = {
+    query: query.query || "",
+    orientation: query.orientation || "portrait",
+    page: query.page || 1,
+    per_page: query.per_page || 15,
+    size: query.size || "small",
+  };
+  const apiUrl = "https://api.pexels.com/v1/search";
+  logger.info(`[NODE][${id}] Request to ${apiUrl}`);
+
+  try {
+    const response: AxiosResponse<any> = await axios.get(apiUrl, {
+      headers: {
+        Authorization: env.PEXELS_API_KEY,
+      },
+      params: requestParams,
+    });
+
+    logObject("info", `[NODE][${id}] Response status 200`, response.data);
+    return res.status(200).json({ success: true, data: response.data });
+  } catch (error) {
+    logObject("error", "[NODE] Response status 500", error);
+    return res
+      .status(500)
+      .json({ success: false, message: INTERNAL_SERVER_ERROR });
+  }
 };
 
 export default searchPicturesByKeyword;
