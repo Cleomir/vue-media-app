5917dbe0fa3a81f6d22e389dc1901725895c36a7
Cleomir <cleomir.mg@hotmail.com>
2020-11-16 00:24:45 +0200

    Add  list pictures route

--- server/src/routes/unsplash/index.ts
+++ server/src/routes/unsplash/index.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 
-import searchPicturesByKeyword from './searchPicturesByKeyword'
+import searchPicturesByKeyword from "./searchPicturesByKeyword";
+import listPictures from "./listPictures";
 
 /**
  * Express router
@@ -9,5 +10,6 @@ const router: Router = express.Router();
 
 // mount routes
 router.get("/search", searchPicturesByKeyword);
+router.get("/pictures", listPictures);
 
 export default router;
