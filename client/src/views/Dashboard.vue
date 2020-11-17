989d35f876ef8ea2f4ca2e3c56d0d2daa203e311
Cleomir <cleomir.mg@hotmail.com>
2020-11-17 23:39:43 +0200

    Add containers

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Sidebar />
+    <Preview />
+  </div>
+</template>
+
+<script>
+import Sidebar from "../components/Sidebar.vue";
+import Preview from "../components/Preview.vue";
+
+export default {
+  components: {
+    Sidebar,
+    Preview,
+  },
+};
+</script>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <Email />
+</template>
+
+<script>
+import Email from "../components/Email.vue";
+
+export default {
+  name: "Login",
+  components: {
+    Email,
+  },
+};
+</script>
