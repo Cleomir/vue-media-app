3622063c03abdb36cd921ccfb003b88580aa641c
Cleomir <cleomir.mg@hotmail.com>
2020-11-17 23:38:12 +0200

    Add global styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
+:root {
+  --primary-blue: #2885e9;
+  --primary-light-blue: #f6f9fc;
+  --text-dark: #4a5a83;
+  --border: #d7dde9;
+}
+
+* {
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
+  background-color: #eee;
+  color: var(--text-dark);
 }
 </style>
