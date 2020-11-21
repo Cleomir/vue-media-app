357afcd80e5ba62b7edddf191a6e382afa4e2201
Cleomir <cleomir.mg@hotmail.com>
2020-11-21 22:19:17 +0200

    Add CORS

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { resolve } from "path";
 import history from "connect-history-api-fallback";
 
 import apiRoutes from "./routes";
-import requestLogger from './middleware/requestLogger'
+import requestLogger from "./middleware/requestLogger";
 
 /**
  * Express instance
@@ -13,11 +14,15 @@ const app: Application = express();
 const publicPath: string = resolve(__dirname, "../../client/dist");
 
 // global middleware
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "25mb",
+  })
+);
+app.use(cors());
 app.use(helmet());
 app.use(express.static(publicPath));
 app.use(requestLogger());
-
 
 // root routes
 app.use("/api", apiRoutes);
