1403fea9cdffbb49f5eeaef6d8582f2b177703ca
Cleomir <cleomir.mg@hotmail.com>
2020-11-22 23:57:58 +0200

    Implement list uploaded pictures route

--- server/src/routes/cloudinary/listUploadedPictures.ts
+++ server/src/routes/cloudinary/listUploadedPictures.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
+import { v2 } from "cloudinary";
+
+import { INTERNAL_SERVER_ERROR } from "../../config/responseErrorMessages";
+import { logObject } from "../../logger";
 
 /**
- *
+ * List 
  * @param req - Request object
  * @param res - Response object
  */
-const listUploadedPictures = (req: Request, res: Response): Response => {
-  return res.status(200).send("Cloudinary list uploaded pictures");
+const listUploadedPictures = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { id } = req;
+  try {
+    const response = await v2.api.resources();
+
+    logObject("info", `[NODE][${id}] Response status 200`, response);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ message: INTERNAL_SERVER_ERROR });
+  }
 };
 
 export default listUploadedPictures;
