b665d5257dad041765d70bd1bc5b3e867a902491
Cleomir <cleomir.mg@hotmail.com>
2020-11-23 14:12:47 +0200

    Add implementation

--- client/src/components/PicturesGrid.vue
+++ client/src/components/PicturesGrid.vue
@@ -1,7 +1,58 @@
 <template>
-  <div>Image grid</div>
+  <div>
+    <ul>
+      <li
+        v-for="(picture, index) in pictures"
+        :key="index"
+        :class="{ 'is-selected': picture === isSelected }"
+        @click="selectPicture(picture)"
+      >
+        <img :src="picture" alt="savedPicture" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapMutations } from "vuex";
+
+export default {
+  props: ["pictures"],
+  data() {
+    return {
+      isSelected: "",
+    };
+  },
+  methods: {
+    ...mapMutations(["setPicturePreview"]),
+    selectPicture(url) {
+      this.isSelected = url;
+      this.setPicturePreview(url);
+    },
+  },
+};
 </script>
+
+<style scoped>
+.is-selected {
+  outline: 3px solid red;
+}
+
+ul {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 50% calc(50% - 10px);
+  list-style: none;
+  margin: 0;
+  padding: 10px 10px 0 10px;
+}
+
+li {
+  cursor: pointer;
+}
+
+ul li img {
+  max-height: 100%;
+  max-width: 100%;
+}
+</style>
