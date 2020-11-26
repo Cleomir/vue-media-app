8703f5e39aa1190e66711633761982d3f34800fe
Cleomir <cleomir.mg@hotmail.com>
2020-11-26 22:42:29 +0200

    Add API URLs

---
+++ client/src/config.js
@@ -0,0 +1,4 @@
+export const apiUrl = "http://localhost:3000";
+export const cloudinaryListPicturesUrl = `${apiUrl}/api/cloudinary/`;
+export const pexelsSearchUrl = `${apiUrl}/api/pexels/search`;
+export const unsplashSearchUrl = `${apiUrl}/api/unsplash/search`;
