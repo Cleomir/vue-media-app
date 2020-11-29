7ab9a78a10e6dc4714a4e006e4fd4fade359e0ae
Cleomir <cleomir.mg@hotmail.com>
2020-11-29 18:31:53 +0200

    Add Cloudinary upload URL

--- client/src/config.js
+++ client/src/config.js
@@ -2,3 +2,4 @@ export const apiUrl = "http://localhost:3000";
 export const cloudinaryListPicturesUrl = `${apiUrl}/api/cloudinary/`;
 export const pexelsSearchUrl = `${apiUrl}/api/pexels/search`;
 export const unsplashSearchUrl = `${apiUrl}/api/unsplash/search`;
+export const uploadPictureUrl = `${apiUrl}/api/cloudinary/upload`;
