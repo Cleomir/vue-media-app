94eb46acc72e005493db72ff4851b838b101dbbf
Cleomir <cleomir.mg@hotmail.com>
2020-11-29 18:32:45 +0200

    Refactor validation rules

--- server/src/validation/RequestValidator.ts
+++ server/src/validation/RequestValidator.ts
@@ -4,30 +4,73 @@ import Joi, { ValidationResult } from "joi";
  * Validate request parameters
  */
 export default class RequestValidator {
-  private static query = Joi.string().trim().min(1).required();
+  private static query = Joi.string().trim().min(1);
   private static page = Joi.number().min(1);
   private static perPage = Joi.number().min(1).max(80);
+  private static unsplashNextPageUrlPattern = Joi.string().pattern(
+    /^https:\/\/api\.unsplash\.com\/search\/photos/
+  );
+  private static pexelsNextPageUrlPattern = Joi.string().pattern(
+    /^https:\/\/api\.pexels\.com\/v1\/search\//
+  );
+  private static baseSearchParamsValidation = {
+    query: RequestValidator.query,
+    page: RequestValidator.page,
+    per_page: RequestValidator.perPage,
+  };
 
   /**
-   * Validate search pictures parameters
+   * Validate Unsplash search pictures parameters
    * @param query keyword to be searched
    * @param page page number
    * @param perPage number of pictures per page
    */
-  public static validateSearchParams(
+  public static validateUnsplashSearchRequest(
     query: string,
     page: number,
-    perPage: number
+    perPage: number,
+    unsplashNextPageUrl?: string
   ): ValidationResult {
     return Joi.object({
-      query: this.query,
-      page: this.page,
-      per_page: this.perPage,
-    }).validate({ query, page, per_page: perPage });
+      ...this.baseSearchParamsValidation,
+      unsplashNextPageUrl: this.unsplashNextPageUrlPattern,
+    })
+      .or("query", "unsplashNextPageUrl")
+      .validate({
+        query,
+        page,
+        per_page: perPage,
+        unsplashNextPageUrl: unsplashNextPageUrl,
+      });
   }
 
   /**
-   * Validate list pictures parameters
+   * Validate Pexels search pictures parameters
+   * @param query keyword to be searched
+   * @param page page number
+   * @param perPage number of pictures per page
+   */
+  public static validatePexelsSearchRequest(
+    query: string,
+    page: number,
+    perPage: number,
+    pexelsNextPageUrl?: string
+  ): ValidationResult {
+    return Joi.object({
+      ...this.baseSearchParamsValidation,
+      pexelsNextPageUrl: this.pexelsNextPageUrlPattern,
+    })
+      .or("query", "pexelsNextPageUrl")
+      .validate({
+        query,
+        page,
+        per_page: perPage,
+        pexelsNextPageUrl: pexelsNextPageUrl,
+      });
+  }
+
+  /**
+   * Validate Unsplash list pictures parameters
    * @param page page number
    * @param perPage number of pictures per page
    */
