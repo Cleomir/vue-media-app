743f81c350ee1a06973547410ab587c6eeaf922b
Cleomir <cleomir.mg@hotmail.com>
2020-11-30 19:37:01 +0200

    Set production API URL's

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const apiUrl = "http://localhost:3000";
+export const apiUrl = process.env.VUE_APP_API_URL || "http://localhost:3000";
 export const cloudinaryListPicturesUrl = `${apiUrl}/api/cloudinary/`;
 export const pexelsSearchUrl = `${apiUrl}/api/pexels/search`;
 export const unsplashSearchUrl = `${apiUrl}/api/unsplash/search`;
