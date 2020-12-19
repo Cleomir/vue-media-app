2f03ae2260c01c910d4bf5868c3b728fd5844ec9
Cleomir <cleomir.mg@hotmail.com>
2020-12-19 11:44:20 +0200

    Add modules config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
+import history from 'connect-history-api-fallback';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from './logger/logger.module';
+import appEnvConfig from './app.config';
+import { RequestLoggerService } from './logger/request-logger/request-logger.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [appEnvConfig], isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '../../client/dist'),
+      exclude: ['/api*'],
+    }),
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(history()).exclude('/api(.*)');
+    consumer.apply(RequestLoggerService).forRoutes('*');
+  }
+}
