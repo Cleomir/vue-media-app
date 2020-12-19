7625345dcee7755db31480a79aad6cd711600f32
Cleomir <cleomir.mg@hotmail.com>
2020-12-19 17:44:19 +0200

    Add middleware config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
+import config from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.use(json({ limit: '20mb' }));
+  app.use(helmet());
+
+  const { SERVER_PORT } = config();
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
