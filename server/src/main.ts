a259b445a5af646c674cb8aa66db857157f89ebc
Cleomir <cleomir.mg@hotmail.com>
2020-12-20 18:55:42 +0200

    Add Cloudinary global config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { json } from 'express';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { v2 } from 'cloudinary';
 
 import { AppModule } from './app.module';
 import config from './app.config';
@@ -9,10 +11,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.use(json({ limit: '20mb' }));
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '25mb' }));
   app.use(helmet());
 
-  const { SERVER_PORT } = config();
+  const {
+    SERVER_PORT,
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
+  } = config();
+  v2.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+  });
   await app.listen(SERVER_PORT);
 }
 bootstrap();
