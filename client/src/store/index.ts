dd47b32905a33f0ba3117eb729664754df88c623
Cleomir <cleomir.mg@hotmail.com>
2021-01-02 17:13:55 +0200

    Add base files

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,44 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    pictures: [] as string[],
+    nextPage: {},
+    picturePreviewUrl: "",
+    displaySpinner: false,
+    displaySnackBar: false,
+    snackBarMessage: "",
+    snackBarType: "",
+  },
+  mutations: {
+    setPictures: (state, pictures) => {
+      state.pictures = [...state.pictures, ...pictures];
+    },
+    clearPictures: (state) => {
+      state.pictures = [];
+      state.nextPage = {};
+    },
+    setNextPage: (state, nextPage) => {
+      state.nextPage = nextPage;
+    },
+    setPicturePreview: (state, previewUrl) => {
+      state.picturePreviewUrl = previewUrl;
+    },
+    showSpinner: (state) => {
+      state.displaySpinner = true;
+    },
+    hideSpinner: (state) => {
+      state.displaySpinner = false;
+    },
+    showSnackBar: (state, options) => {
+      state.snackBarMessage = options.message;
+      state.snackBarType = options.type;
+      state.displaySnackBar = true;
+    },
+    hideSnackBar: (state) => {
+      state.displaySnackBar = false;
+    },
+  },
   actions: {},
-  modules: {}
+  modules: {},
 });
